a856f78f92626789beb7b796c9d2ed1bf2c90b26
zhulinski <a.zhulinski@gmail.com>
2024-10-27 13:47:43 +0200

    test without sheet.connect.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { GoogleSheets } from './entities/google.sheets.entity';
 import { SheetsService } from './services/sheets.service';
 import { SheetController } from './controllers/sheets.controller';
 import { SheetsGateways } from './sheet/sheets.gateways';
-import { SheetConnectService } from './services/sheet.connect.service';
+// import { SheetConnectService } from './services/sheet.connect.service';
 import { configService } from './services/config.service';
 import { SubscribersController } from './controllers/subscribers.controller';
 import { SubscribersService } from './services/subscribers.service';
