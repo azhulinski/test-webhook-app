ba7d5aa4a5d2c0a8289894fd6ac299067542635b
zhulinski <a.zhulinski@gmail.com>
2024-10-29 17:08:09 +0200

    small changes

--- src/decorators/logging.decorator.ts
+++ src/decorators/logging.decorator.ts
@@ -8,6 +8,7 @@ const logger: Logger = new Logger('Logging');
     descriptor.value = async function(...args: any[]) {
       const result = await originalMethod.apply(this, args);
       logger.log(`Method ${propertyKey} has been invoked and responded with result '${JSON.stringify(result)}'`);
+      return result;
     };
   };
 }
